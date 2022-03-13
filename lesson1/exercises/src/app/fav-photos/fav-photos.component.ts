3bb60380ba6db081d067de224751defb1346d1b1
Your Name <nujiyediin@gmail.com>
2022-03-12 21:38:17 -0600

    did part 1 of part 1 exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Beautiful pics of me';
+  image1 = 'https://pbs.twimg.com/profile_images/1007725154057707520/i_t7isD8_400x400.jpg';
+  image2 = 'https://npr.brightspotcdn.com/dims4/default/5c67227/2147483647/strip/true/crop/200x267+0+32/resize/300x400!/format/webp/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Fa6%2Fc7%2F7b720fea43f18d8ef78768b2f306%2Fnomin-ujiyediin.jpg';
+  image3 = 'https://pbs.twimg.com/profile_images/1007725154057707520/i_t7isD8_400x400.jpg';
 
   constructor() { }
 
